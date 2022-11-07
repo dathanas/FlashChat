0be88dec9952da65550754cd2cf90f41a98d4706
Despina Athanasleri <39651792+dathanas@users.noreply.github.com>
2022-11-07 16:05:05 +0200

    Add files via upload

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Athanasleri, Despoina on 22/7/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
